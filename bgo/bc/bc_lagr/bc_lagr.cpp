9ae4f4f40bd22e49df3045e46a4cfd83f0167337
Kevin Nobel <k.nobel@utwente.nl>
2024-05-01 22:53:43 +0200

    add LAGraph betweenness centrality wrapper bgo

---
+++ bgo/bc/bc_lagr/bc_lagr.cpp
@@ -0,0 +1,14 @@
+
+
+#include "GraphBLAS.h"
+#include "LAGraph.h"
+
+#include "datastructures.hpp"
+
+#include "bc_lagr.hpp"
+
+char msg[LAGRAPH_MSG_LEN];
+
+int betweenness_centrality_LAGr(LAGraph_Graph G, CArray<GrB_Index> sources, GrB_Vector *centrality) {
+    return LAGr_Betweenness(centrality, G, sources.data, sources.size, msg);
+}
