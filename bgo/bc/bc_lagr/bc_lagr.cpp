cca9af88eedeca5283d619d3344163cc940d1c49
Kevin Nobel <k.nobel@utwente.nl>
2024-05-03 14:05:36 +0200

    compute transpose matrix if needed for LAGraph

--- bgo/bc/bc_lagr/bc_lagr.cpp
+++ bgo/bc/bc_lagr/bc_lagr.cpp
@@ -10,5 +10,7 @@
 char msg[LAGRAPH_MSG_LEN];
 
 int betweenness_centrality_LAGr(LAGraph_Graph G, CArray<GrB_Index> sources, GrB_Vector *centrality) {
+    if (G->AT == NULL)
+        LAGraph_Cached_AT(G, msg);
     return LAGr_Betweenness(centrality, G, sources.data, sources.size, msg);
 }
