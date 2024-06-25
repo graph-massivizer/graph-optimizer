84fb18fc81e0248b61dd81ed66e315d300d97300
Kevin Nobel <k.nobel@utwente.nl>
2024-06-25 22:23:57 +0200

    split find_max bgo in two

--- bgo/find_max/find_max/find_max.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FIND_MAX_HPP
-#define FIND_MAX_HPP
-
-#include "datastructures.hpp"
-
-int find_max_index_CA(CArray<int> values, int *index);
-int find_max_index_GV(GrB_Vector values, GrB_Index *index);
-
-#endif
--- bgo/find_max/find_max_gb/find_max_gb.cpp
+++ bgo/find_max/find_max_gb/find_max_gb.cpp
@@ -1,23 +1,9 @@
-#include <cstddef>
-
 #include "GraphBLAS.h"
 
-#include "datastructures.hpp"
+#include "find_max_gb.hpp"
 
-#include "find_max.hpp"
 
-int find_max_index_CA(CArray<int> values, int *index) {
-    int max_value = 0;
-    for (size_t i = 0; i < values.size; i++) {
-        if (values.data[i] <= max_value) { continue; }
-
-        max_value = values.data[i];
-        *index = i;
-    }
-    return 0;
-}
-
-int find_max_index_GV(GrB_Vector values, GrB_Index *index) {
+int find_max_index_GB(GrB_Vector values, GrB_Index *index) {
     GrB_Index nvals;
     GrB_Vector_nvals(&nvals, values);
     int value;
