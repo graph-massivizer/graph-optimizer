fb07bd55f98578ea231dd1651bbd3533df6c5313
Duncan Bart <d.bart@utwente.nl>
2024-08-06 15:37:30 +0200

    api greenifier added

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import json
 import subprocess
 import pathlib
+from datetime import datetime
 
 prediction_script = str(pathlib.Path(__file__).parent.parent.absolute()) + '/models/prediction.py'
 print(prediction_script)
@@ -19,6 +20,22 @@ def predict(request, evaluate):
     output = subprocess.check_output(['python3', prediction_script, hardware, bgo_dag])
     return output
 
+def greenify_output(output):
+    greenifier_output = {"tasks": json.loads(output)}
+
+    for bgo in greenifier_output['tasks']:
+        bgo['runTimes'] = {perf['host']: perf['runtime'] for perf in bgo['performances']}
+        bgo['energyConsumption'] = {perf['host']: perf['energy'] for perf in bgo['performances']}
+        bgo['submissionTime'] = datetime.today().strftime('%Y-%m-%d')
+        # TODO: These values are hardcoded for now
+        bgo['cpuUsage'] = 10000
+        bgo['memCapacity'] = 1000000
+        bgo['cpuCount'] = 1
+
+        del bgo['performances']
+
+    return greenifier_output
+
 
 @app.route('/models', methods=['POST'])
 def models():
@@ -28,3 +45,8 @@ def models():
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
     return predict(request, True)
+
+@app.route('/greenifier', methods=['POST'])
+def greenifier():
+    output = predict(request, True)
+    return greenify_output(output)
